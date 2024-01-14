d80a7e75e5dd51feb588b4137b0a5718e403161c
Saadz505 <156594366+Saadz505@users.noreply.github.com>
2024-01-14 19:16:17 +0400

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,34 @@
+const wordList = [
+    {
+        word: "heartless",
+        hint: "A song that reminds me of you."
+    },
+    {
+        word: "rave",
+        hint: "which app did we meet on?"
+    },
+    {
+        word: "instagram",
+        hint: "The name of the application i uses most?"
+    },
+    {
+        word: "sea",
+        hint: "Most of the pictures I take are about the ...."
+    },
+    {
+        word: "training",
+        hint: "The habit that I do every day in my day?"
+    },
+    {
+        word: "football",
+        hint: "My favorite hobby?"
+    },
+    {
+        word: "moonlight",
+        hint: "The light from the moon?"
+    },
+    {
+        word: "yup",
+        hint: "Do i love you?"
+    },
+];
